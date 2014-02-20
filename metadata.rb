c0fa88a9b8f7c6f6585a5c262b02ec5f43f6e867
Jeremy Shipman <jeremy@burnbright.net>
2014-02-20 17:14:59 +1300

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vimtaku@gmail.com'
 license          'Apache 2.0'
 description      'This script is forked from https://github.com/alexism/cloudwatch_monitoring. cloudwatch_monitoring installs the Amazon CloudWatch Monitoring Scripts for Linux - custom metrics that reports memory, swap, and disk space utilization metrics.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "1.3.0"
+version "1.3.1"
 
 %w{ ubuntu debian redhat centos amazon }.each do |os|
   supports os
